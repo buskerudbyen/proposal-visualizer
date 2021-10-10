deabc3c1c227790187354b208ae66779a2cdd376
Leonard Ehrenfried <mail@leonard.io>
2021-10-10 20:40:06 +0200

    Paint lines

---
+++ ui/app.js
@@ -0,0 +1,53 @@
+var map = new maplibregl.Map({
+  container: 'map', // container id
+  style: {
+    'version': 8,
+    'sources': {
+      'raster-tiles': {
+        'type': 'raster',
+        'tiles': [
+          'https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=a09616e6150b4c0fa635a23efcda2af8'
+        ],
+        'tileSize': 256,
+        'attribution':
+        'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
+      }
+    },
+    'layers': [
+      {
+        'id': 'simple-tiles',
+        'type': 'raster',
+        'source': 'raster-tiles',
+        'minzoom': 0,
+        'maxzoom': 22
+      }
+    ]
+  },
+  center: [10.4051, 59.6153],
+  zoom: 15
+});
+
+map.on('load', function () {
+  map.addSource('tippecanoe', {
+    'type': 'vector',
+    'tiles': [ 'https://byvekstavtale.leonard.io/{z}/{x}/{y}.pbf' ],
+    'minzoom': 6,
+    'maxzoom': 14
+  });
+
+  map.addLayer({
+    'id': 'cycleway',
+    'type': 'line',
+    'source': 'tippecanoe',
+    'source-layer': 'cycleway',
+    'layout': {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    'paint': {
+      'line-opacity': 1,
+      'line-color': '#1965e8',
+      'line-width': 6
+    }
+  });
+});
