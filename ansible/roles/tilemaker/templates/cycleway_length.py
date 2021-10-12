2f34b0aae25f3005fd60ac2773224e6382affcaf
Leonard Ehrenfried <mail@leonard.io>
2021-10-12 13:31:33 +0200

    Improve length calculation

--- ansible/roles/tilemaker/templates/cycleway_length.py
+++ ansible/roles/tilemaker/templates/cycleway_length.py
@@ -1,36 +1,52 @@
 from flask import Flask
 import osmium as o
-import sys
+import sys, os
+
 app = Flask(__name__)
 
-PROPOSED_FILE="/tmp/buskerudbyen/cycleway-proposed.pbf"
+FILENAME=os.environ.get("PBF_FILE", "/var/tilemaker/norway-filtered.pbf")
 
 @app.route('/')
-def hello_world():
-    return { "proposed": distance() }
+def get_distances():
+    return distance()
 
 
 class RoadLengthHandler(o.SimpleHandler):
     def __init__(self):
         super(RoadLengthHandler, self).__init__()
-        self.length = 0.0
+        self.proposed_length = 0.0
+        self.existing_length = 0.0
 
     def way(self, w):
-        if 'buskerudbyen:cycleway' in w.tags:
+        if w.tags.get("buskerudbyen:cycleway") == "existing":
             try:
-                self.length += o.geom.haversine_distance(w.nodes)
+                self.existing_length += o.geom.haversine_distance(w.nodes)
             except o.InvalidLocationError:
                 # A location error might occur if the osm file is an extract
                 # where nodes of ways near the boundary are missing.
                 print("WARNING: way %d incomplete. Ignoring." % w.id)
 
+        elif w.tags.get("buskerudbyen:cycleway") == "proposed":
+            try:
+                self.proposed_length += o.geom.haversine_distance(w.nodes)
+            except o.InvalidLocationError:
+                # A location error might occur if the osm file is an extract
+                # where nodes of ways near the boundary are missing.
+                print("WARNING: way %d incomplete. Ignoring." % w.id)
+
+
+
 def distance():
     h = RoadLengthHandler()
     # As we need the geometry, the node locations need to be cached. Therefore
     # set 'locations' to true.
-    h.apply_file(PROPOSED_FILE, locations=True)
+    h.apply_file(FILENAME, locations=True)
 
-    return h.length
+    return {
+        "proposed": h.proposed_length,
+        "existing": h.existing_length,
+        "total": h.proposed_length + h.existing_length
+    }
 
 if __name__ == '__main__':
    app.run()
